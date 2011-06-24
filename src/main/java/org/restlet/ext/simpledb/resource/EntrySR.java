5fb8b7ade187130091f0c6696ac1d6638c0ac95a
Andrei.Pozolotin <Andrei.Pozolotin@gmail.com>
2011-06-24 17:17:03 -0500

    a

--- src/main/java/org/restlet/ext/simpledb/resource/EntrySR.java
+++ src/main/java/org/restlet/ext/simpledb/resource/EntrySR.java
@@ -1,7 +1,24 @@
 package org.restlet.ext.simpledb.resource;
 
+import org.restlet.representation.Representation;
+import org.restlet.resource.Get;
+import org.restlet.resource.Put;
 import org.restlet.resource.ServerResource;
 
 public class EntrySR extends ServerResource {
+
+	@Get("json")
+	public Representation load() {
+
+		return null;
+
+	}
+
+	@Put("json")
+	public void save(Representation entity) throws Exception {
+
+		String entry = entity.getText();
+
+	}
 
 }
